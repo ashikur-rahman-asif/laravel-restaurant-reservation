2e690066f636c336150b68ad2c39a02be7d6563d
asif247 <ashikurrahmanasif247@yahoo.com>
2023-12-26 21:21:54 +0600

    admin middleware set

---
+++ resources/js/Pages/Admin/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <h1 className='text-center flex items-center justify-center'>Haw maw khaw</h1>
+        </div>
+    );
+};
+
+export default Index;
